8fb09c39c9a219a476f8ef90e245faf0708b11e6
mohammadhossein <example@gmai.com>
2025-08-17 15:04:48 +0330

    update

--- VenomX/__main__.py
+++ VenomX/__main__.py
@@ -44,16 +44,6 @@ async def init():
     await userbot.start()
     await Ayush.start()
     LOGGER("VenomX").info("Assistant Started Sucessfully")
-    try:
-        await Ayush.stream_call(
-            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
-        )
-    except NoActiveGroupCall:
-        LOGGER("VenomX").error(
-            "Please ensure the voice call in your log group is active."
-        )
-        exit()
-
     await Ayush.decorators()
     LOGGER("VenomX").info("VenomX Started Successfully")
     await idle()
